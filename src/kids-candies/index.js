69cb3a6939a96627ff4498b0cfaef52dd53dfe57
Mostafa Salama <mostafalearning2016@gmail.com>
2020-06-26 20:49:33 +0200

    add solution for the probelm

---
+++ src/kids-candies/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+let kidsWithCandies = function (candies, extraCandies) {
+    let maxValue = candies[0];
+    candies.forEach((value) => {
+        if (value > maxValue) maxValue = value;
+    });
+    const output = [];
+    for (let candy of candies) {
+        const isEnough = maxValue - candy <= extraCandies;
+        output.push(isEnough);
+    }
+    return output;
+};
