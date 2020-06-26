7bd4a374effe3480a6146440748fa45c196b995c
Mostafa Salama <mostafalearning2016@gmail.com>
2020-06-26 21:00:30 +0200

    add the solution of the problem

---
+++ src/jewels-and-stones/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+let numJewelsInStones = function(J, S) {
+        let jewelsCount = 0 ;
+        for(let j of J) {
+            for (let s of S) {
+                if (j === s) {
+                    jewelsCount +=1
+                }
+            }
+        }
+        return   jewelsCount ;
+};
+
