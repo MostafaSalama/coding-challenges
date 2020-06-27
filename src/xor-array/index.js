7a85cbf93c9238b8a49c0bee6abe029c43c15d07
Mostafa Salama <mostafalearning2016@gmail.com>
2020-06-27 09:39:50 +0200

    add solution for the problem

---
+++ src/xor-array/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @param {number} start
+ * @return {number}
+ */
+let xorOperation = function(n, start) {
+        const nums = [] ;
+        for(let i = 0 ;i<n;i++) {
+            nums[i] = start + 2 * i ;
+        }
+        let result = nums[0] ;
+        for(let i = 1 ; i<n;i++) {
+            result = result ^ nums[i]  ;
+        }
+        return  result ;
+};
+module.exports = xorOperation ;
